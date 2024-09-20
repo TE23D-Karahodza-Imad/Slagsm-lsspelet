5880218eccb8772ccf9b897c3e837b1ae78431aa
Imad Karahodza <imadahmed.karahodza@elev.ga.ntig.se>
2024-09-20 10:55:27 +0200

    sadasd

--- Slagssmålet/Program.cs
+++ Slagssmålet/Program.cs
@@ -1,2 +1,28 @@
-﻿Console.WriteLine("Welcome to my game where you will fight alongside another player");
-Console.WriteLine("T")
+﻿int hp1 = 100;
+int hp2 = 100;
+string playerblue;
+string playerred;
+
+
+//------------------------------------//
+Console.WriteLine("Welcome to my game where you will fight alongside another player");
+Console.WriteLine("Please, two of you should type in your names:");
+//--//
+Console.Write("Player Blue: ");
+playerblue = Console.ReadLine();
+//--//
+Console.Write("Player Red: ");
+playerred = Console.ReadLine(); 
+//--//
+
+
+
+
+
+
+
+
+
+
+//------------------------------------//
+Console.ReadLine();

9072187eb5080c768bfd3c0b7ba1b4918e8622df
Imad Karahodza <imadahmed.karahodza@elev.ga.ntig.se>
2024-09-27 10:35:15 +0200

    Lätta delen klart

--- Slagssmålet/Program.cs
+++ Slagssmålet/Program.cs
@@ -1,11 +1,11 @@
-﻿using System.Security.Cryptography;
-
-int hp1 = 100;
-int hp2 = 100;
+﻿int hpBlue = 100;
+int hpRed = 100;
 string playerBlue;
 string playerRed;
 
-Random generator = new Random();
+Random random = new Random();
+
+
 
 //------------------------------------//
 Console.WriteLine("Welcome to my game where you will fight alongside another player");
@@ -15,13 +15,43 @@ Console.Write("Player Blue: ");
 playerBlue = Console.ReadLine();
 //--//
 Console.Write("Player Red: ");
-playerRed = Console.ReadLine(); 
+playerRed = Console.ReadLine();
 //--//
-for (int i = 1; i <= 10; i++)
+
+
+while (hpBlue > 0 || hpRed > 0)
 {
-    double result = Math.Pow(5,i);
-    Console.WriteLine(result);
+    //Player blue punching
+    int damage1 = random.Next(10, 30);
+    Console.WriteLine("It is Blues turn to punch so.. - press enter.");
+    Console.ReadLine();
+    hpRed -= damage1;
+    Console.WriteLine("Reds HP left:" + hpRed);
+    //
+    if (hpRed <= 0)
+    {
+        Console.WriteLine("Red has died and Blue won the game.. Congratulations!");
+        Console.ReadLine();
+        break;
+    }
+
+    //Player red punching
+    int damage2 = random.Next(10, 30);
+    Console.WriteLine("It is Reds turn to slay Blue! - press enter.");
+    Console.ReadLine();
+    hpBlue -= damage2;
+    Console.WriteLine("Blues HP left:" + hpBlue);
+
+    if (hpBlue <= 0)
+    {
+        Console.WriteLine("Blue has died and Red won the game.. Congratulations!");
+        Console.ReadLine();
+        break;
+    }
+
+
 }
+
 
 
 
